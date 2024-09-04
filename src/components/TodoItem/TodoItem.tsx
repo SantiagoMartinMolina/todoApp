d11fd324dfcc0e16ebeff43980fe1b89e0847d65
SantiagoMartinMolina <molina.santiago.martin@gmail.com>
2024-09-04 03:30:54 -0300

    Add gesture handler

--- src/components/TodoItem/TodoItem.tsx
+++ src/components/TodoItem/TodoItem.tsx
@@ -1,11 +1,15 @@
-import { View, Pressable } from "react-native";
+import { View, Pressable, Animated } from "react-native";
 import React, { useState } from "react";
 import { ToDo } from "../../types/types";
 import { FontAwesome } from "@expo/vector-icons";
 import styles from "./styles";
 // import { useSafeAreaFrame } from "react-native-safe-area-context";
 import { Button, Dialog, Portal, Text } from "react-native-paper";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
+import {
+  GestureHandlerRootView,
+  PanGestureHandler,
+  State,
+} from "react-native-gesture-handler";
 
 interface Props {
   item: ToDo;
@@ -13,29 +17,100 @@ interface Props {
   onDelete: (id: string) => void;
 }
 
+// definir el umbral de desplazamiento para decidir cuando activar las acciones asignadas al swipe (swipe to delete, swipe top toggle)
+
+const TODO_SWIPE_THRESHOLD = 100;
+
 const TodoItem = ({ onDelete, onToggle, item }: Props) => {
   //   const frame = useSafeAreaFrame();
   //   const textWidth = frame.width - 16 - 16 - 24 - 20 - 20; // ver como mejorar la definicion de esta constante
   const [isVisibleDialog, setIsVisibleDialog] = useState<boolean>(false);
   const hideDialog = () => setIsVisibleDialog(!isVisibleDialog);
   const showDialog = () => setIsVisibleDialog(true);
+
+  // Vamos a declarar un valor animado (Animated.Value). --> controla la posicion horizontal del elemento (cada Todo) durante el swipe. Lo vamos a inicializar en 0 (posicion original)
+  // Se actualiza el valor de forma autonoma durante el gesto
+
+  const translateX = new Animated.Value(0);
+
+  /** el evento que maneja el movimiento del swipe en tiempo real se llama 'onGestureEvent' --> va a ir actualizando el valor de translateX durante el gesto.
+   */
+
+  const onGestureEvent = Animated.event(
+    [{ nativeEvent: { translationX: translateX } }],
+    { useNativeDriver: true }
+  );
+
+  /**
+   * 'onHandlerStateChange' va a ser crucial para definir que sucede cuando se completa el gesto de swipe
+   */
+
+  const onHandlerStateChange = (event: any) => {
+    // Solamente se actuara cuando el swipe haya finalizado.
+    if (event.nativeEvent.state === State.END) {
+      const { translationX } = event.nativeEvent;
+      // Si el desplazamiento hacia la izquierda supera el umbral que definimos para este movimiento concreto, se considera una accion de eliminacion
+      if (translationX < -TODO_SWIPE_THRESHOLD * 2.5) {
+        onDelete(item.id);
+      }
+
+      // Si el desplazamiento hacia la derecha supera el umbral que definimos para este movimiento concreto, se considera una accion de toggleCompleted
+      else if (translationX > TODO_SWIPE_THRESHOLD) {
+        onToggle(item.id);
+      }
+
+      Animated.spring(translateX, {
+        toValue: 0, //volver a la posicion inicial
+        useNativeDriver: true,
+        friction: 5,
+      }).start();
+    }
+  };
+
+  const animatedStyle = {
+    transform: [{ translateX }],
+  };
+
+  const redLayerOpacity = translateX.interpolate({
+    inputRange: [-TODO_SWIPE_THRESHOLD * 2.5, 0],
+    outputRange: [1, 0],
+  });
+
   return (
     <GestureHandlerRootView>
-      <View style={[styles.itemContainer, styles.mb]}>
-        <Pressable onLongPress={() => onToggle(item.id)} onPress={showDialog}>
-          <Text
-            style={[
-              styles.taskText,
-              item.isCompleted && styles.completed,
-              // { maxWidth: textWidth },
-            ]}
-          >
-            {item.description.slice(0, 25)}...
-          </Text>
-        </Pressable>
-        <Pressable onPress={() => onDelete(item.id)}>
-          <FontAwesome name="trash" size={24} />
-        </Pressable>
+      <View style={[styles.mb, styles.container]}>
+        {/* Una capa de presentacion que aparece detras del Todo en color rojo */}
+        <Animated.View style={[styles.redLayer, { opacity: redLayerOpacity }]}>
+          <Animated.View style={styles.deleteIconContainer}>
+            <FontAwesome name="trash" size={24} />
+          </Animated.View>
+        </Animated.View>
+
+        {/* Este contenedor maneja el swipe y aplica las transformaciones animadas definidas en animatedStyle */}
+        <PanGestureHandler
+          onGestureEvent={onGestureEvent} // Vincula el movimiento del gesto a la actualizacion de la variable translateX
+          onHandlerStateChange={onHandlerStateChange} // Maneja el estado final del gesto para determinar si borramos, marcamos o no hacemos nada
+        >
+          <Animated.View style={[styles.itemContainer, animatedStyle]}>
+            <Pressable
+              onLongPress={() => onToggle(item.id)}
+              onPress={showDialog}
+            >
+              <Text
+                style={[
+                  styles.taskText,
+                  item.isCompleted && styles.completed,
+                  // { maxWidth: textWidth },
+                ]}
+              >
+                {item.description.slice(0, 25)}...
+              </Text>
+            </Pressable>
+            <Pressable onPress={() => onDelete(item.id)}>
+              <FontAwesome name="trash" size={24} />
+            </Pressable>
+          </Animated.View>
+        </PanGestureHandler>
         <Portal>
           <Dialog visible={isVisibleDialog} onDismiss={hideDialog}>
             <Dialog.Icon icon="information-outline" size={34} color="red" />
--- src/components/TodoItem/styles.ts
+++ src/components/TodoItem/styles.ts
@@ -1,6 +1,10 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
+    container: {
+        position: 'relative',
+        // backgroundColor: "blue"
+    },
     itemContainer: {
         flexDirection: "row",
         justifyContent: "space-between",
@@ -19,7 +23,26 @@ const styles = StyleSheet.create({
     completed: {
         textDecorationLine: "line-through",
         color: "tomato"
+    },
+
+    redLayer: {
+        position: "absolute",
+        borderRadius: 15,
+        backgroundColor: "red",
+        right: 0,
+        left: 0,
+        bottom: 0,
+        top: 0
+        // width: "100%",
+        // height: "100%"
+    },
+
+    deleteIconContainer: {
+        position: "absolute",
+        right: 20,
+        top: 10
     }
+
 
 });
 
